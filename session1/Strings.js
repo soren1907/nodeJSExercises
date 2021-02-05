02404c11c249b2ceac028262f2abd782a5a40967
soren1907 <sorenraben@hotmail.com>
2021-02-05 13:58:15 +0100

    opgave 1 og 2

---
+++ session1/Strings.js
@@ -0,0 +1,69 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+const addedNumbers3 = parseFloat(numberOne) + parseFloat(numberTwo);
+console.log(addedNumbers3);
+
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+const addedNumbers4 = parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo);
+
+console.log(addedNumbers4.toFixed(2));
+
+
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+const sum = one + two + three;
+const avg = sum/3;
+console.log(avg.toFixed(5));
+
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+const letter = letters.charAt(2);
+console.log(letter);
+
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+
+const replace = fact.replace("javascript!", "Javascript!");
+console.log(replace);
+
+
+// capitalize the J in Javascript
+
+
+// --------------------------------------
+
+
+
---
+++ session1/test.js
@@ -0,0 +1,29 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constants
+
+const firstName = "Anders";
+const lastName = "Latif";
+
+console.log('My firstname is ' + firstName + 'and my lastname is ' + lastName);
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2020";
+const number = 1;
+
+const currentYear = parseInt(year) + number;
+
+console.log(currentYear)
+// Add the year plus the number
+// The result should be 2021
+// You cannot touch line 1 or 2
+
+
+// --------------------------------------
